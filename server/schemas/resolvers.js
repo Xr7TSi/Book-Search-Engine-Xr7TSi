dda28b7c7ecd1ce863a6639c55614a3c588154a7
Jonathan Rein <jrein1296@gmail.com>
2021-07-10 20:54:46 -0400

    Correction for userByID querry in typeDefs.js.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,25 +3,27 @@ const { Book, User } = require('../models');
 const resolvers = {
   Query: {
     book: async () => {
-      return Book.find({});
+      return await Book.find({});
     },
     user: async () => {
-      return User.find({});
+      return await User.find({});
+    },
+    userById: async (parent, args) => {
+      return await User.findById(args.id);
     }
-
   },
 
   Mutation: {
     addBook: async (parent, { title }) => {
-      return Book.create({ title });
+      return await Book.create({ title });
     },
    
     removeBook: async (parent, { ID }) => {
-      return Book.findOneAndDelete({ _id: ID });
+      return await Book.findOneAndDelete({ _id: ID });
     },
 
     addUser: async (parent, { username, email, password, savedBooks }) => {
-      return User.create({ username, email, password, savedBooks });
+      return await User.create({ username, email, password, savedBooks });
     },
    
   },
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -14,6 +14,7 @@ const typeDefs = gql`
   type Query {
     book: [Book]!
     user: [User]!
+    userById(id: ID!): User
   }
 
   type User {
