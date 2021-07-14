9f73befc78bf4039228edce635b09780bcbaf0f1
Jonathan Rein <jrein1296@gmail.com>
2021-07-13 20:20:48 -0400

    Updates to auth.js.  Adding startApolloServer function to server/server.js.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -27,16 +27,15 @@ const typeDefs = gql`
 
   type Query {
     me: User
-    getAllUsers: [User]
+    users: [User]
   }
 
   type Mutation {
-    login(username: String!, password: String!): Auth
-    addUser(username: String!, email: String!, password: String!, savedBooks: [String]): User
-    saveBook(_id: ID!, authors: String, description: String, bookId: String, image: String, link: String, title: String): Book
-    removeBook(bookId: String): User
-    
-  }
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(authors: [String], description: String, title: String, bookId: String, image: String, link: String): User
+    removeBook(bookId: String!): User
+}
 `;
 
 module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -1,26 +1,24 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
-const { authMiddleware } = require('./utils/auth');
-
 const db = require('./config/connection');
-const routes = require('./routes');
-
+const { authMiddleware } = require('./utils/auth');
 const PORT = process.env.PORT || 3001;
-const app = express();
 
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
-});
+async function startApolloServer() {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+  })
+  await server.start();
+  const app = express();
+  server.applyMiddleware({ app });
+  
 
-server.applyMiddleware({ app });
-
-
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
@@ -29,14 +27,16 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
-// db.once('open', () => {
-//   app.listen(PORT, () => {console.log(`🌍 Now listening on localhost:${PORT}`));
-// });
-
-db.once('open', () => {
+await db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
+return {server, app};
+}
+startApolloServer();
